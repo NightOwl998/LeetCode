02be4a587b3dc6bdb098d2421c75accfd9660fcb
NightOwl998 <64656757+NightOwl998@users.noreply.github.com>
2022-12-04 22:25:56 +0100

    Time: 2132 ms (9.98%), Space: 404.7 MB (10.01%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -1,13 +1,12 @@
 class Solution {
-    public String frequencySort(String s) {
-       
+    public String frequencySort(String s) {       
         Map<Character,Integer> map=new HashMap<>();
         for(int i=0;i<s.length();i++)
         {
             if(map.containsKey(s.charAt(i))) map.replace(s.charAt(i),map.get(s.charAt(i))+1); 
             else map.put(s.charAt(i),1);
         }
-        //System.out.println("the map is"+map);
+       
         
         String res="";
         while(!map.isEmpty())
@@ -22,13 +21,7 @@ class Solution {
             
         }
         
-      /*  for(Map.Entry<Character,Integer> frequency:map.entrySet())
-        {
-            for(int j=0;j<frequency.getValue();j++)
-            {
-                res=res+frequency.getKey();
-            }
-        }*/
+   
         return res;
     }
 }
